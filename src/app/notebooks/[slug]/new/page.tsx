e8426eaa056e274ed943da563a4b9ecb677f01fb
wisechimp <17533346+wisechimp@users.noreply.github.com>
2024-09-30 13:16:57 +0200

    feat: Created the principal paths e will use in the project
    We also created a new note page, although this may be combined with the note page

---
+++ src/app/notebooks/[slug]/new/page.tsx
@@ -0,0 +1,14 @@
+// This may be combinable with the noteId page
+const NewNotebookPage = () => {
+  return (
+    <div>
+      <h1>Notebook 4 - A New Note...</h1>
+      <p>
+        Yeah so this could be combined with the note page as it's basically the
+        same, just an empty note waiting to be edited...
+      </p>
+    </div>
+  )
+}
+
+export default NewNotebookPage
