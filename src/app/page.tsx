938b43c0e0e35653371cc937a5d1f5e72d1081c8
wisechimp <17533346+wisechimp@users.noreply.github.com>
2024-10-04 14:15:10 +0200

    feat: Add a Header component which contains the page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
+import Header from "@/components/header/header"
 import Link from "next/link"
 
 const Home = () => {
   return (
-    <main>
-      <div>
-        <h1>Welcome to Diginote!</h1>
-        <p>
-          Here are <Link href='/notebooks/toadslist'>some notebooks</Link>.
-        </p>
-        <p>
-          Here is a link to your <Link href='/profile'>Profile</Link>
-        </p>
-      </div>
-    </main>
+    <div>
+      <Header pageTitle='Welcome to Diginote!' />
+      <p>
+        Here are <Link href='/notebooks'>some notebooks</Link>.
+      </p>
+    </div>
   )
 }
 
