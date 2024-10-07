d4edfea0aa00835783b7123fc941234fa9f67bd2
wisechimp <17533346+wisechimp@users.noreply.github.com>
2024-10-07 09:52:15 +0200

    feat: Add icons to Menu

--- src/siteData/menu.ts
+++ src/siteData/menu.ts
@@ -1,15 +1,22 @@
+import { FaHome } from "react-icons/fa"
+import { MdManageAccounts } from "react-icons/md"
+import { SlNotebook } from "react-icons/sl"
+
 const MenuData = [
   {
     title: "Home",
     link: "/",
+    Icon: FaHome,
   },
   {
     title: "Notebooks",
     link: "/notebooks",
+    Icon: SlNotebook,
   },
   {
     title: "Profile",
     link: "/profile",
+    Icon: MdManageAccounts,
   },
 ]
 
---
+++ src/types/MenuItemType.ts
@@ -0,0 +1,9 @@
+import { IconType } from "react-icons"
+
+type MenuItemType = {
+  title: string
+  link: string
+  Icon: IconType
+}
+
+export default MenuItemType
