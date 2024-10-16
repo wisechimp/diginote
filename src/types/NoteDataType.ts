eb87205c8ca7307048ca5345e8d4aa8943c5d61f
wisechimp <17533346+wisechimp@users.noreply.github.com>
2024-10-16 10:56:16 +0200

    refactor: Extract NoteDataType
    As it's needed in more than one place now

---
+++ src/types/NoteDataType.ts
@@ -0,0 +1,11 @@
+type NoteDataType = {
+  id: number
+  title: string
+  content: string
+  user_id: number
+  notebook_id: number
+  created_at: number
+  updated_at: number
+}
+
+export default NoteDataType
